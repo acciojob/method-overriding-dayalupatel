e81ecc343fda5c9a8d8044a0494d4607373e28c1
dayalu <dayalupatel98@gmail.com>
2022-10-11 15:53:05 +0530

    code2 added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,22 +2,19 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    B obj = new B();
+    B b = new B();
 
-    String ans1 = obj.meth();
-    System.out.println(ans1);
-
-    String ans2 = obj.meth();
-    System.out.println(ans2);
+    b.meth();
 
   }
 
-public class A {
+public static class A {
     public String meth() {
         return "Invoking method from class A";
     }
  }
-public class B extends A {
+public static class B extends A {
+    @Override
     public String meth() {
         return "Method is overridden in Extendend class B";
     }
